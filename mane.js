4f4818e2bc0f4da3b1a4faad9b2c2669cd7f811f
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-13 21:50:40 +0300

    История никнеймов, добавил в историю, поменял ник

--- mane.js
+++ mane.js
@@ -1,3 +1,13 @@
-const array = [1, 33, -10, 'example', 'word'];
+// const array = [1, 33, -10, 'example', 'word'];
+//
+// console.log(array.join('_'));
 
-console.log(array.join('_'));
+const user = {
+    name: 'Alex',
+    nickname: 'Alexxx675',
+    nicknameHistory: ['AlexLuk', 'Xela-123', 'Sasha 8'],
+};
+
+
+user.nicknameHistory.push(user.nickname);
+user.nickname = 'AlexYx'
