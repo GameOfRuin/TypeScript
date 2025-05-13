665f0513cf15ad855a31253fd9d284476941018c
Anatoly Kochetov <tolik.kochetov44@gmail.com>
2025-05-13 22:00:44 +0300

    Бросок кубиков

--- mane.js
+++ mane.js
@@ -2,12 +2,26 @@
 //
 // console.log(array.join('_'));
 
-const user = {
-    name: 'Alex',
-    nickname: 'Alexxx675',
-    nicknameHistory: ['AlexLuk', 'Xela-123', 'Sasha 8'],
-};
+// const user = {
+//     name: 'Alex',
+//     nickname: 'Alexxx675',
+//     nicknameHistory: ['AlexLuk', 'Xela-123', 'Sasha 8'],
+// };
+//
+//
+// user.nicknameHistory.push(user.nickname);
+// user.nickname = 'AlexYx';
 
 
-user.nicknameHistory.push(user.nickname);
-user.nickname = 'AlexYx'
+
+let x = Math.round(Math.random()*5 +1);
+let y = Math.round(Math.random()*5 +1);
+let count = 0
+
+while (!(x === y)){
+    x = Math.round(Math.random()*5 +1);
+    y = Math.round(Math.random()*5 +1);
+    count++
+}
+
+console.log(count);
